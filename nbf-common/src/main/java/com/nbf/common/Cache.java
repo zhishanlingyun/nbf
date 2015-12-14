62ab627c518d551a1173bbf58c7b762a3e484fde
zhishanlingyun <liuli307@sina.com>
2015-12-14 23:20:02 +0800

    nbf

---
+++ nbf-common/src/main/java/com/nbf/common/Cache.java
@@ -0,0 +1,16 @@
+package com.nbf.common;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 15-11-26
+ * Time: 上午11:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class Cache {
+
+    public String get(String key){
+        return "cache";
+    }
+
+}
