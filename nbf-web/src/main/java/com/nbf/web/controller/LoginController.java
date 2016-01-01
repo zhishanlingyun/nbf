8441a6a92b753a436ac1fc4bc5105e0a31d84903
zhishanlingyun <liuli307@sina.com>
2016-01-01 16:18:20 +0800

    样式

--- nbf-web/src/main/java/com/nbf/web/controller/LoginController.java
+++ nbf-web/src/main/java/com/nbf/web/controller/LoginController.java
@@ -20,7 +20,7 @@ public class LoginController {
         ModelAndView mv = new ModelAndView("");
         mv.addObject("username","张三");
         //mv.setViewName("x/login");
-        mv.setViewName("q/form");
+        mv.setViewName("q/login");
         //mv.setViewName("m/index");
         return mv;
     }
